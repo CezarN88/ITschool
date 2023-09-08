414cf53473c803d0015605cc1d970103778bbfb1
Cezar Nedelcu <cezarnedelcu@gmail.com>
2023-09-08 21:19:59 +0300

    session3

---
+++ src/session3/practice/relationalOperators.java
@@ -0,0 +1,13 @@
+package session3.practice;
+
+public class relationalOperators {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 30;
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber == secondNumber);
+    }
+}
