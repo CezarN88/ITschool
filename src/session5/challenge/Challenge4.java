daa37c7aa4a9db72ef0b8c70053da3e7c0da8549
Cezar Nedelcu <cezarnedelcu@gmail.com>
2023-09-17 07:49:31 +0300

    challenges 1-3

---
+++ src/session5/challenge/Challenge4.java
@@ -0,0 +1,17 @@
+package session5.challenge;
+
+import java.util.Scanner;
+
+public class Challenge4 {
+
+    //4. String Reverser
+    //Ask the user to enter a string. Print the reversed version of this string.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String inputString = scanner.nextLine();
+
+    }
+}
