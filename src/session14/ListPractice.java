2e3982d5152984f839e308cee8b978dc91ba5878
Cezar Nedelcu <cezarnedelcu@gmail.com>
2023-10-17 21:37:08 +0300

    session14 practice

---
+++ src/session14/ListPractice.java
@@ -0,0 +1,64 @@
+package session14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPractice {
+
+    public static void main(String[] args) {
+        List<Integer> myList = initializeList();
+        System.out.println(myList);
+
+        List<Integer> updatedList = getUpdateList(myList, 1, 10);
+        System.out.println(updatedList);
+
+        List<Integer> evenNumbers = getEvenNumbersList(myList);
+        System.out.println(evenNumbers);
+
+        int maxValue = findMaximum(myList);
+        System.out.println(String.format("Max value is: %d" , maxValue));
+    }
+
+    private static int findMaximum(List<Integer> list) {
+        if (list == null || list.isEmpty()){
+            System.out.println("The list is empty");
+        }
+        int maxValue = list.get(0);
+        for ( int number : list){
+            if ( number > maxValue) {
+                maxValue = number;
+            }
+        }
+        return maxValue;
+    }
+
+    private static List<Integer> getEvenNumbersList (List<Integer> list) {
+        List<Integer> evenNumbers = new ArrayList<>();
+        for (int number : list) {
+            if (number % 2 == 0) {
+                evenNumbers.add(number);
+            }
+        }
+        return evenNumbers;
+    }
+
+    private static List<Integer> initializeList() {
+        List<Integer> list = new ArrayList<>();
+
+        for (int index = 0; index < 10; index++) {
+            list.add(index);
+        }
+        return list;
+    }
+
+    private static List<Integer> getUpdateList(List<Integer> list, int index, int value) {
+        if (index >= list.size() || index < 0) {
+            System.out.println("Invalid index");
+            return list;
+        }
+
+        List<Integer> result = new ArrayList<>(list);
+        result.set(index, value);
+        return result;
+    }
+}
